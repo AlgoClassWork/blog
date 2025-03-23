3eab91ac8dfc67cf896cbb5eaf9cdf25334288f0
AlgoClassWork <kyxec.play@gmail.com>
2025-03-23 12:32:27 +0600

    Add files via upload

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -16,7 +17,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -32,8 +36,8 @@ class Post(models.Model):
         default=Status.DRAFT
     )
 
-    objects = models.Manager()  # The default manager.
-    published = PublishedManager()  # Our custom manager.
+    objects = models.Manager()  
+    published = PublishedManager()  
 
     class Meta:
         ordering = ['-publish']
@@ -43,6 +47,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -64,4 +80,4 @@ class Comment(models.Model):
         ]
 
     def __str__(self):
-        return f'Комментарий от {self.name} для {self.post}'
+        return f'Comment by {self.name} on {self.post}'
